b4010f32e75796319fbbddb0085f6a3aee82569c
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-10-09 15:26:13 +0100

    Added api v1 to URL

--- chicken-api/src/endpoints/chicken.ts
+++ chicken-api/src/endpoints/chicken.ts
@@ -7,8 +7,10 @@ type ChickensRequest = {
   skip?: number;
 };
 
+const BASE_URL = "/api/v1";
+
 export const addChickensEndpoint = (app: Express) => {
-  app.get("/chickens", async (req, res) => {
+  app.get(`${BASE_URL}/chickens`, async (req, res) => {
     const { limit = 10, skip = 0 } = req.query as ChickensRequest;
     console.log({ limit, skip });
     const myChickens: Chicken[] = (chickens as Chicken[]).slice(skip, limit);
