5667990080e0d8a9d9db71989b4f5f1ef1dfd4fd
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-10-14 11:39:44 +0100

    Made paging work correctly

--- chicken-api/src/endpoints/chicken.ts
+++ chicken-api/src/endpoints/chicken.ts
@@ -13,7 +13,10 @@ export const addChickensEndpoint = (app: Express) => {
   app.get(`${BASE_URL}/chickens`, async (req, res) => {
     const { limit = 10, skip = 0 } = req.query as ChickensRequest;
     console.log({ limit, skip });
-    const myChickens: Chicken[] = (chickens as Chicken[]).slice(skip, limit);
+    const myChickens: Chicken[] = (chickens as Chicken[]).slice(
+      skip,
+      limit + skip
+    );
     res.send(myChickens);
   });
 };
